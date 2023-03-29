95703a228edc4d9c2ae3b0ee6c2d57eb830a5e34
seyi4mi <seyi4mi99@gmail.com>
2023-03-29 15:18:35 +0800

    added navbar links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
 import React from 'react';
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const navbar = () => {
+  return (
+    <header className="w-full px-32 py-8 font-medium">
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About Us</Link>
+        <Link href="/contact">Contact Us</Link>
+        <Link href="/properties">Our Properties</Link>
+      </nav>
+    </header>
+  );
 };
 
-export default Navbar;
+export default navbar;
