98738063d413cbc3cf8d6a1731952f4e4b2c4aaf
seyi4mi <seyi4mi99@gmail.com>
2023-04-03 21:04:39 +0800

    fix the dark bg

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Layout = ({ children, className = '' }) => {
   return (
     <div
-      className={`w-full h-fit inline-block z-0 p-32 dark:bg-dark  ${className}`}
+      className={`w-full h-fit inline-block z-0 p-32 bg-light  ${className}`}
     >
       {children}
     </div>
