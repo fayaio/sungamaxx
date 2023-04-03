c6bef67877984ecc804362d16e5ba9c0dc9ef34c
seyi4mi <seyi4mi99@gmail.com>
2023-04-03 19:49:15 +0800

    made footer fully responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,18 @@ const Footer = () => {
     <footer
       className="w-full  mt-auto border-t-2 border-solid
     text-lg 
+    
     "
     >
-      <Layout className="py-8 !flex !text-center! h-fit text-darkgrey bg-footerbg !items-center justify-between !self-center  text-pcolor text-base font-mont">
+      <Layout
+        className="p-8 w-full text-sm padding !flex !text-center! h-fit text-darkgrey bg-footerbg !items-center justify-between !self-center 
+       text-pcolor  font-mont md:flex-col md:text-center sm:text-xs "
+      >
         <span>{new Date().getFullYear()} &copy; All Rights Reserved </span>
 
-        <Link href="/">Sungamax Nigeria Ltd</Link>
+        <Link href="/" className="md:pt-4">
+          Sungamax Nigeria Ltd
+        </Link>
       </Layout>
     </footer>
   );
