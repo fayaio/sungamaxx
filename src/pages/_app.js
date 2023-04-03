5794a93d738d6e7ad17d03d9c0e837bd2c177727
seyi4mi <seyi4mi99@gmail.com>
2023-04-03 20:37:47 +0800

    added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,8 +13,32 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Sungamax</title>
+        <meta
+          name="description"
+          content="To Elevate The Spiritual And Financial Well-Being Of Humanity"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#b84195" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <main
