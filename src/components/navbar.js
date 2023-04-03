2e6be932a7c7282132fb32a69d9633a72a1a34fc
seyi4mi <seyi4mi99@gmail.com>
2023-04-03 21:11:44 +0800

    kept space beteen dark and block

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -79,7 +79,7 @@ const NavBar = () => {
           }`}
         ></span>
         <span
-          className={`bg-darkblock transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+          className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
             isOpen ? 'opacity-0' : 'opacity-100'
           }`}
         ></span>
