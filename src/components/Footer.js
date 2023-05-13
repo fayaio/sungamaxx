47649a44709523c842baef12d9192753813c82b4
seyi4mi <seyi4mi99@gmail.com>
2023-05-13 20:11:48 +0800

    changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Layout from './Layout';
 import Link from 'next/link';
-
+import { FaFacebook } from 'react-icons/fa';
+import { AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai';
+AiFillInstagram;
 const Footer = () => {
   return (
     <footer
@@ -11,16 +13,23 @@ const Footer = () => {
     "
     >
       <Layout
-        className="p-8 w-full text-sm padding !flex !text-center! h-fit text-darkgrey bg-footerbg !items-center justify-between !self-center 
-       text-pcolor  font-mont md:flex-col md:text-center sm:text-xs "
+        className="p-8 w-full  padding !flex !text-center! h-fit text-darkgrey bg-footerbg !items-center justify-between !self-center 
+       text-pcolor  font-light text-sm  font-mont md:flex-col md:text-center sm:text-xs "
       >
-        <span>{new Date().getFullYear()} &copy; All Rights Reserved </span>
+        <div className="flex flex-col">
+          <div className="md:pt-4  flex flex-row gap-1">
+            <Link href={''}> Privacy Policy </Link>|
+            <Link href={''}> Cookie Policy</Link>|
+            <Link href={''}> Refund Policy</Link>
+          </div>
+          <span className="pt-4">Sungamax Nigeria Ltd Copyright&copy; </span>
+        </div>
 
-        <Link href="/" className="md:pt-4">
-          Sungamax Nigeria Ltd
-        </Link>
-
-        <div className=""></div>
+        <div className="flex gap-3 text-xl md:pt-4">
+          <FaFacebook />
+          <AiFillInstagram />
+          <AiFillTwitterCircle />
+        </div>
       </Layout>
     </footer>
   );
